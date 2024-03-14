5f47900043678c8d1840db79a63b3f8e44db0139
juhalo <107361221+juhalo@users.noreply.github.com>
2024-03-14 23:52:19 +0200

    Reduce redundancy

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const dict = {
     2: "On",
 };
 
+const sources = {
+    "Normal": "./assets/audio.mp3",
+    "Rachel": "./assets/Rachel.mp3",
+    "Brian": './assets/Brian.mp3',
+    "Choir": './assets/Kuoro_k.mp3',
+    "Robot": './assets/Robo_k.mp3',
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     var audio = document.getElementById('myAudio');
     var playButton = document.getElementById('playButton');
@@ -14,6 +22,31 @@ document.addEventListener('DOMContentLoaded', function () {
     var kuoroButton = document.getElementById('kuoroButton');
     var audioSource = document.getElementById('audioSource');
     var rangeInput = document.getElementById("rangeInput");
+    const buttons = document.querySelectorAll('.btn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            currentTime = audio.currentTime;
+            normalButton.classList.remove('green')
+            rachelButton.classList.remove('green')
+            brianButton.classList.remove('green')
+            kuoroButton.classList.remove('green')
+            robotButton.classList.remove('green')
+
+            button.classList.add('green')
+
+            rangeInput.value = 1;
+            document.getElementById("rangevalue").textContent = dict[rangeInput.value];
+    
+            var isPaused = audio.paused;
+    
+            audioSource.src = sources[button.textContent];
+            audio.load();
+            if (!isPaused) {
+                audio.play();
+            }
+        });
+    });
 
     rangeInput.addEventListener("change", function() {
         document.getElementById("rangevalue").textContent = dict[rangeInput.value];
@@ -46,115 +79,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    normalButton.addEventListener('click', function () {
-        currentTime = audio.currentTime;
-        normalButton.classList.add('green')
-        rachelButton.classList.remove('green')
-        brianButton.classList.remove('green')
-        kuoroButton.classList.remove('green')
-        robotButton.classList.remove('green')
-        rangeInput.value = 1;
-        document.getElementById("rangevalue").textContent = dict[rangeInput.value];
-
-        var isPaused = audio.paused;
-
-        audioSource.src = './assets/audio.mp3';
-        audio.load();
-        if (!isPaused) {
-            audio.play();
-        }
-    });
-
-    rachelButton.addEventListener('click', function () {
-        currentTime = audio.currentTime;
-        normalButton.classList.remove('green')
-        brianButton.classList.remove('green')
-        kuoroButton.classList.remove('green')
-        robotButton.classList.remove('green')
-        rachelButton.classList.add('green')
-        rangeInput.value = 1;
-        document.getElementById("rangevalue").textContent = dict[rangeInput.value];
-
-        var isPaused = audio.paused;
-
-        audioSource.src = './assets/Rachel.mp3'
-        audio.load();
-        if (!isPaused) {
-            audio.play();
-        }
-    });
-
-    brianButton.addEventListener('click', function () {
-        currentTime = audio.currentTime;
-        normalButton.classList.remove('green')
-        rachelButton.classList.remove('green')
-        kuoroButton.classList.remove('green')
-        robotButton.classList.remove('green')
-        brianButton.classList.add('green')
-        rangeInput.value = 1;
-        document.getElementById("rangevalue").textContent = dict[rangeInput.value];
-
-        var isPaused = audio.paused;
-
-        audioSource.src = './assets/Brian.mp3'
-        audio.load();
-        if (!isPaused) {
-            audio.play();
-        }
-    });
-
-    kuoroButton.addEventListener('click', function () {
-        currentTime = audio.currentTime;
-        normalButton.classList.remove('green')
-        rachelButton.classList.remove('green')
-        brianButton.classList.remove('green')
-        robotButton.classList.remove('green')
-        kuoroButton.classList.add('green')
-        rangeInput.value = 1;
-        document.getElementById("rangevalue").textContent = dict[rangeInput.value];
-
-        var isPaused = audio.paused;
-
-        audioSource.src = './assets/Kuoro_k.mp3'
-        audio.load();
-        if (!isPaused) {
-            audio.play();
-        }
-    });
-
-    robotButton.addEventListener('click', function () {
-        currentTime = audio.currentTime;
-        normalButton.classList.remove('green')
-        rachelButton.classList.remove('green')
-        brianButton.classList.remove('green')
-        kuoroButton.classList.remove('green')
-        robotButton.classList.add('green')
-        rangeInput.value = 1;
-        document.getElementById("rangevalue").textContent = dict[rangeInput.value];
-
-        var isPaused = audio.paused;
-
-        audioSource.src = './assets/Robo_k.mp3'
-        audio.load();
-        if (!isPaused) {
-            audio.play();
-        }
-    });
-
     audio.addEventListener('loadedmetadata', function() {
-        // Set current time to the saved time
         audio.currentTime = currentTime;
     });
 });
 
 function showPage(pageId) {
-    // Hide all pages
     var pages = document.querySelectorAll('.page');
     pages.forEach(function(page) {
         page.classList.remove('active-page');
     });
 
-    // Show the selected page
     var selectedPage = document.getElementById(pageId);
     selectedPage.classList.add('active-page');
 }
